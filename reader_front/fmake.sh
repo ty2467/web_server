a38c76fbbf59fbd0bded22d373895086412a0fdd
Phoenix Web <rembrandt@pstvserver.phoenixtv-us.local>
2026-09-09 18:46:30 -0400

    invocation correctly

--- reader_front/fmake.sh
+++ reader_front/fmake.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 ENV_FILE=~/.env
 web_deployment=$(sed -nE 's/^web_deployment[[:space:]]*=[[:space:]]*(.*)$/\1/p' "$ENV_FILE")
-clear_destination = "${web_deployment}/dl.sh"
-"clear_destination"
+clear_destination="${web_deployment}/dl.sh"
+"$clear_destination"
 
 #build
 ng build --configuration production
--- writer_front/fmake.sh
+++ writer_front/fmake.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 ENV_FILE=~/.env
 cms_deployment=$(sed -nE 's/^cms_deployment[[:space:]]*=[[:space:]]*(.*)$/\1/p' "$ENV_FILE")
-clear_destination = "${cms_deployment}/dl.sh"
-"clear_destination"
+clear_destination="${cms_deployment}/dl.sh"
+"$clear_destination"
 
 #build
 ng build --configuration production
